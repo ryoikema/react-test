fc67dcf2312853553d266791e2c710cb671c927f
ryoikema <ryoikema@gmail.com>
2019-12-07 03:43:33 +0900

    Todoのrenderメソッドを定義

--- 04_todo/src/todo.js
+++ 04_todo/src/todo.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react';
 
 class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      name: ''
+    };
+  }
 
+  render() {
+    const { todos } = this.state;
+
+    return (<div>
+      <input type="text" />
+      <button>登録</button>
+      <ul>
+        {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+      </ul>
+    </div>);
+  }
 }
 
 export default Todo;
