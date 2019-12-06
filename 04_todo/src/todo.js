24fe0ae75df18a4615b1bb61a7cffa0a539be4da
ryoikema <ryoikema@gmail.com>
2019-12-07 03:52:29 +0900

    todo追加メソッド（テキストフィールドの値を保存、登録ボタンの処理）

--- 04_todo/src/todo.js
+++ 04_todo/src/todo.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+//実際に入力された値はe.target.valueで取り出せる
+//setState()でstateを保存する
+
 
 class Todo extends Component {
   constructor(props) {
@@ -9,12 +12,27 @@ class Todo extends Component {
     };
   }
 
+  //テキストフィールドに入力された値を保存する
+  onInput = e => {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  //登録ボタンをクリックした際の処理
+  addTodo = () => {
+    const { todos, name } = this.state
+    this.setState({
+      todos: [...todos, name]
+    })
+  }
+
   render() {
     const { todos } = this.state;
 
     return (<div>
-      <input type="text" />
-      <button>登録</button>
+      <input type="text" onInput={this.onInput} />
+      <button onClick={this.addTodo}>登録</button>
       <ul>
         {todos.map((todo, index) => <li key={index}>{todo}</li>)}
       </ul>
